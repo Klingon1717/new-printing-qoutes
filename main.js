63f4d422102d1906d6440df6355514547635d9da
Whitney Murray <wnksmith92@yahoo.com>
2015-11-16 17:07:17 -0600

    added comments

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+//I declared qoute to hold the prompts info so I can pull that info when needed.
+//I decalred speak so I can whold the authors name and pull it when I need it.
+//I console.log to add the author the string says plus the qoute!
+
 var qoute = prompt('What\'s the qoute\?');
 console.log(qoute);
 var speak = prompt('Who spoke such qoute\?');
 console.log(speak);
 
 console.log(speak + "says" + qoute);
+
+//I think I did it correctlly whewwww!!!
